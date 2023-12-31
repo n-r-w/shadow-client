01535e8224fa826f2639804422203b15286c96ba
nrw <nrew@yandex.ru>
2024-01-01 01:36:43 +0300

    ping

--- wireguard/ping.sh
+++ wireguard/ping.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-main_pid=$1
-
-_term() {
-  echo "Ping script stopped"
-  exit 0
-}
-
-trap _term SIGTERM
-
 while true
 do    
-    random_delay=$((MIN_PING_DELAY + RANDOM % (MAX_PING_DELAY - MIN_PING_DELAY + 1)))    
-    ping -c 1 ${PING_HOST}
+    random_delay=$((MIN_PING_DELAY + RANDOM % (MAX_PING_DELAY - MIN_PING_DELAY + 1)))
+    ping -c 1 ${PING_HOST} 2>&1 > /dev/null    
     sleep ${random_delay}
-
-    if ! kill -0 "$main_pid" 2>/dev/null; then        
-        exit 0
-    fi
 done
